c898667e3fa7a9da117708821662487ffd1df3fb
shrinefrost <surajpandey1115@gmail.com>
2025-12-29 11:48:23 +0530

    adding 4.3 How to handle static files like css and image

---
+++ 4.3 EJS Partials/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+/* Write your code here:
+Step 1: Render the home page "/" index.ejs
+Step 2: Make sure that static files are linked to and the CSS shows up.
+Step 3: Add the routes to handle the render of the about and contact pages.
+  Hint: Check the nav bar in the header.ejs to see the button hrefs
+Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+//step 2: Main work from server other is link files:
+app.use(express.static("public"));
+
+//step 1:
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
+});
+
+//step3:
+app.get("/about", (req, res) => {
+  res.render(__dirname + "/views/about.ejs");
+});
+//step 4:
+app.get("/contact", (req, res) => {
+  res.render(__dirname + "/views/contact.ejs");
+});
